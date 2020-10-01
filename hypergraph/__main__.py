c6473ddfc73a9bf12ea79c6e4418fb7e994e91ae
Anton Eriksson <anton.eriksson@umu.se>
2020-10-01 22:14:31 +0200

    Change option --bipartite-backtracking -> --bipartite-non-backtracking

--- hypergraph/__main__.py
+++ hypergraph/__main__.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument("--self-links", const=True, default=False, action="store_const")
-    parser.add_argument("--bipartite-backtracking", const=True, default=True, action="store_const")
+    parser.add_argument("--bipartite-non-backtracking", const=True, default=False, action="store_const")
     parser.add_argument("--shifted", const=True, default=False, action="store_const")
     parser.add_argument("filename", type=FileType("r"), default=sys.stdin)
 
     args = parser.parse_args()
 
     main(args.filename,
-         backtracking=args.bipartite_backtracking,
+         backtracking=not args.bipartite_non_backtracking,
          self_links=args.self_links,
          shifted=args.shifted)
