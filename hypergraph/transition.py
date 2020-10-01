7114b7ebaf3e48551c410f621bfc5eced9f586ef
Anton Eriksson <anton.eriksson@umu.se>
2020-10-01 14:36:20 +0200

    Cleanup transition.py

--- hypergraph/transition.py
+++ hypergraph/transition.py
@@ -6,56 +6,51 @@ from hypergraph.io import Node, HyperEdge, Weight
 
 def E(edges: Iterable[HyperEdge]):
     print("[E] pre-calculating... ", end="")
-    edges_ = defaultdict(list)
+    edges_ = defaultdict(set)
 
     for edge, nodes, _ in edges:
         for node in nodes:
-            edges_[node.id].append(edge)
+            edges_[node.id].add(edge)
 
     edges_ = dict(edges_)
     print("done")
 
-    def inner(v: Node, u: Node = None):
-        if not u:
-            return set(edges_[v.id])
-
-        return {edge.id for edge in edges
-                if {u, v} <= edge.nodes}
+    def inner(v: Node):
+        return edges_[v.id]
 
     return inner
 
 
 def d(edges: Iterable[HyperEdge]):
-    E_v = E(edges)
+    E_ = E(edges)
 
     def inner(v: Node):
-        return sum(edge.omega for edge in edges
-                   if edge.id in E_v(v))
+        return sum(omega for edge, _, omega in edges
+                   if edge in E_(v))
 
     return inner
 
 
 def delta(weights: Iterable[Weight]):
     print("[delta] pre-calculating... ", end="")
-    gamma_tot = {}
+    delta_ = defaultdict(float)
 
-    for weight in weights:
-        if weight.edge not in gamma_tot:
-            gamma_tot[weight.edge] = 0
-        gamma_tot[weight.edge] += weight.gamma
+    for edge, _, gamma in weights:
+        delta_[edge] += gamma
 
+    delta_ = dict(delta_)
     print("done")
 
     def inner(e: HyperEdge):
-        return gamma_tot[e.id]
+        return delta_[e.id]
 
     return inner
 
 
 def gamma(weights: Iterable[Weight]):
     print("[gamma] pre-calculating... ", end="")
-    gamma_ = {(weight.edge, weight.node.id): weight.gamma
-              for weight in weights}
+    gamma_ = {(edge, node.id): gamma_
+              for edge, node, gamma_ in weights}
     print("done")
 
     def inner(e: HyperEdge, v: Node):
@@ -64,43 +59,31 @@ def gamma(weights: Iterable[Weight]):
     return inner
 
 
-def step_1(weights: Iterable[Weight], self_links=False):
-    gamma_ev = gamma(weights)
-    delta_e = delta(weights)
-
-    def inner(e: HyperEdge, u: Node, v: Node):
-        if self_links:
-            return gamma_ev(e, v) / (delta_e(e) - gamma_ev(e, u))
-
-        return gamma_ev(e, v) / delta_e(e)
-
-    return inner
-
-
-def step_2(edges: Iterable[HyperEdge]):
-    d_v = d(edges)
-
-    def inner(e: HyperEdge, u: Node):
-        return e.omega / d_v(u)
-
-    return inner
-
-
 def p(edges: Iterable[HyperEdge], weights: Iterable[Weight], self_links=False, shifted=False):
-    step_1_ = step_1(weights, self_links)
-    step_2_ = step_2(edges)
+    gamma_ = gamma(weights)
+    delta_ = delta(weights)
+    d_ = d(edges)
+
+    def p_node(e: HyperEdge, u: Node, v: Node):
+        if self_links:
+            return gamma_(e, v) / (delta_(e) - gamma_(e, u))
+
+        return gamma_(e, v) / delta_(e)
+
+    def p_edge(e: HyperEdge, u: Node):
+        return e.omega / d_(u)
 
     def inner(u: Node, e1: HyperEdge, v: Node, e2: HyperEdge):
         if shifted:
             if v not in e1.nodes:
                 return 0
 
-            return step_1_(e1, u, v) * step_2_(e2, v)
+            return p_node(e1, u, v) * p_edge(e2, v)
 
         else:
             if u not in e2.nodes:
                 return 0
 
-            return step_1_(e2, u, v) * step_2_(e2, u)
+            return p_node(e2, u, v) * p_edge(e2, u)
 
     return inner
