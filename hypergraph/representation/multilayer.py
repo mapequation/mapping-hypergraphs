8d5d33c305ae0e7f265ea0c371c69b2b0e5a0389
Anton Eriksson <anton.eriksson@umu.se>
2020-10-05 12:35:45 +0200

    Don't sort multilayer intra/inter links separately

--- hypergraph/representation/multilayer.py
+++ hypergraph/representation/multilayer.py
@@ -1,9 +1,8 @@
 from itertools import product
-from typing import List
 
 from hypergraph.io import HyperGraph
 from hypergraph.transition import p
-from network import MultiLayerLink, MultilayerNetwork
+from network import MultilayerNetwork
 
 
 def create_network(hypergraph: HyperGraph, self_links: bool) -> MultilayerNetwork:
@@ -13,26 +12,21 @@ def create_network(hypergraph: HyperGraph, self_links: bool) -> MultilayerNetwor
 
     p_ = p(edges, weights, self_links)
 
-    intra = []
-    inter = []
+    links = []
 
     for e1, e2 in product(edges, edges):
         for u, v in product(e1.nodes, e2.nodes):
             if not self_links and u == v:
                 continue
 
-            w = p_(e1, u, e2, v)
+            weight = p_(e1, u, e2, v)
 
-            if w < 1e-10:
+            if weight < 1e-10:
                 continue
 
-            if e1 == e2:
-                intra.append((e1.id, u.id, e2.id, v.id, w))
-            else:
-                inter.append((e1.id, u.id, e2.id, v.id, w))
+            links.append((e1.id, u.id, e2.id, v.id, weight))
 
-    links: List[MultiLayerLink] = [((e1, u), (e2, v), w)
-                                   for links in (intra, inter)
-                                   for e1, u, e2, v, w in sorted(links, key=lambda link: link[0])]
+    links = [((e1, u), (e2, v), w)
+             for e1, u, e2, v, w in sorted(links)]
 
     return MultilayerNetwork(nodes, links)
