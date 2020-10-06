e8a52b5db6ba2fa5ecd038436019be89e98c95ca
Anton Eriksson <anton.eriksson@umu.se>
2020-10-06 15:59:23 +0200

    Add --outfile option

--- hypergraph/main.py
+++ hypergraph/main.py
@@ -1,3 +1,4 @@
+from os import path
 from typing import Callable, Optional
 
 from infomap import Infomap
@@ -38,6 +39,7 @@ def run_infomap(filename,
 
 def run(file,
         outdir="output",
+        outfile=None,
         multilayer=False,
         bipartite=False,
         bipartite_non_backtracking=False,
@@ -51,8 +53,8 @@ def run(file,
     if multilayer:
         network = representation.multilayer(hypergraph, self_links)
 
-        file_ending = "_self_links" if self_links else ""
-        filename = "{}/{}{}".format(outdir, "multilayer", file_ending)
+        basename = outfile if outfile else "multilayer"
+        basename += "_self_links" if self_links else ""
 
         def set_network(im: Infomap):
             im.set_names(network.nodes)
@@ -61,8 +63,8 @@ def run(file,
     elif bipartite or bipartite_non_backtracking:
         network = representation.bipartite(hypergraph, bipartite_non_backtracking)
 
-        file_ending = "_non_backtracking" if bipartite_non_backtracking else ""
-        filename = "{}/{}{}".format(outdir, "bipartite", file_ending)
+        basename = outfile if outfile else "bipartite"
+        basename += "_non_backtracking" if bipartite_non_backtracking else ""
 
         def set_network(im: Infomap):
             im.set_names(network.nodes)
@@ -76,9 +78,9 @@ def run(file,
     elif clique_graph or directed_clique:
         network = representation.clique(hypergraph, directed_clique, self_links)
 
-        file_ending = "_directed" if directed_clique else ""
-        file_ending += "_self_links" if self_links else ""
-        filename = "{}/{}{}".format(outdir, "clique", file_ending)
+        basename = outfile if outfile else "clique"
+        basename += "_directed" if directed_clique else ""
+        basename += "_self_links" if self_links else ""
 
         def set_network(im: Infomap):
             im.add_nodes(network.nodes)
@@ -86,6 +88,8 @@ def run(file,
 
     else:
         return
+
+    filename = path.join(outdir, basename)
 
     if write_network:
         network.write(filename + ".net")
@@ -131,6 +135,7 @@ def main():
                         help="only search for two-level partitions")
     parser.add_argument("--no-infomap", action="store_true", help="do not run Infomap")
     parser.add_argument("-s", "--seed", default=123, type=int, help="random seed")
+    parser.add_argument("-o", "--outfile")
 
     output = parser.add_argument_group("representation")
     options = output.add_mutually_exclusive_group(required=True)
